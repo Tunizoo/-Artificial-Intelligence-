6e13c104f15b3970c7b5fb173796c03b4d671b9f
Tunizoo <nyzarmhiri@gmail.com>
2017-02-14 09:34:07 +0100

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,52 @@
+public class Fact() {
+	Name subject;
+	Name relation;
+	Name object;
+	//Ex: Cat(subject) Eat(relation) Mouse (object)
+	Fact condition;//condition for the fact to be true (ex: Usual, if Night, etc)
+	public Fact not() {return(Fact(subject,relation.not(),object));}
+	public Fact and(Fact other){
+		if(this.is(other)||other.is(this)) return(this);
+		if(this.not().is(other)||other.not().is(this)) return(FALSE);
+		Fact result=new Fact;		
+		if(object==null&&other.object==null||object.equal(other.object)) {
+			result.object=object;
+			if(subject.equal(other.subject)) {
+				result.subject=subject;
+				result.relation=relation.and(other.relation));
+			}
+			else if(relation.equal(other.relation)) {
+				result.relation=relation;
+				result.subject=subject.and(other.subject));
+			}
+			else {
+				result.subject=Name(this);
+				result.relation=AND;
+				result.object=Name(other);
+			}
+		}
+		else if(subject==null&&other.subject==null||subject.equal(other.subject)) {
+			result.subject=subject;
+			if(object.equal(other.object)) {
+				result.object=object;
+				result.relation=relation.and(other.relation));
+			}
+			else if(relation.equal(other.relation)) {
+				result.relation=relation;
+				result.object=object.and(other.object));
+			}
+			else {
+				result.subject=Name(this);
+				result.relation=AND;
+				result.object=Name(other);
+			}
+		}
+		else {
+			result.subject=Name(this);
+			result.relation=AND;
+			result.object=Name(other);
+		}		
+	}
+	public Fact or(Fact other);
+
+}
---
+++ Name.java
@@ -0,0 +1,32 @@
+public class Name {
+	String expression;
+	Name root;//ex: Elephant is Animal
+	Name[] properties;//ex: Vegetarian
+	public boolean equal(Name other){
+		if(expression==other.expression) return(true);
+		/*******************/
+		return(false);
+	}
+	public is(Name other){
+		Name r=this;
+		while(r.root!=null){
+			if(r.root.equal(other)) return(true);
+			r=r.root;
+		}
+		if(root.is(other.root))
+		{//are the properties of this included in those of other?
+			for(short i=0;i<properties.length;i++){
+				short j=0;
+				while(!properties[i].is(other.properties[j])) j++;
+				if(j==other.properties.length) return(false);
+			} return(true);
+		}	
+		return(false);		
+	}
+	public Name not(){
+		//if(expression.charAt(0)=='!')
+	}
+	public Name and(Name name);
+	public Name or(Name name);
+	public Name of(Name name);	
+}
